3964fc7a099e2d28a50ff4680ab3f9e336acd7d3
Yaroslav L <yariklaz@yandex.ru>
2025-03-24 15:09:31 +0300

    Step_03: Peoples_project

--- First/config.py
+++ First/config.py
@@ -1,5 +1,13 @@
 class Config:
-    PORT = 5000
+    TESTING = True
+
 
 class ProductionConfig(Config):
-    DEBUG = FalseS
+    DEBUG = False
+    SERVER_NAME = "0.0.0.0:8888"
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SERVER_NAME = "0.0.0.0:5555"
+    PORT = 3333
--- First/run.py
+++ First/run.py
@@ -1,4 +1,13 @@
 from my_app import app
+from config import DevelopmentConfig
+
+
+#print(app.default_config) #конфигурация по умолчанию
+app.config.from_object("config.DevelopmentConfig")
+
+#print("\n================ after loading ================\n")
+#print(app.config) #словарь настроек для запуска 
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=DevelopmentConfig.PORT)
