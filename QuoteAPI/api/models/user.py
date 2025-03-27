27728ee569d6b8feea45fe4eb2de605060b52a7d
Yaroslav L <yariklaz@yandex.ru>
2025-03-27 14:45:16 +0300

    Step_33: Forms added - examples/form_example.py

--- QuoteAPI/api/models/user.py
+++ QuoteAPI/api/models/user.py
@@ -1,10 +1,7 @@
 from passlib.apps import custom_app_context as pwd_context
 from api import app, db
-from config import Config
 import sqlalchemy.orm as so
 import sqlalchemy as sa
-# from itsdangerous import URLSafeSerializer
-# from itsdangerous import BadSignature
 import jwt
 from time import time
 
@@ -33,8 +30,6 @@ class UserModel(db.Model):
         return pwd_context.verify(password, self.password_hash)
     
     def generate_auth_token(self):
-        # s = URLSafeSerializer(Config.SECRET_KEY)
-        # return s.dumps({'id': self.id})
         token = jwt.encode({'id': self.id, 'exp': int(time()+3600)},
                            key=app.config['SECRET_KEY'],
                            algorithm='HS256')
@@ -52,14 +47,4 @@ class UserModel(db.Model):
         return user
 
 
-    # @staticmethod
-    # def verify_auth_token(token):
-    #     s = URLSafeSerializer(Config.SECRET_KEY)
-    #     try:
-    #         data = s.loads(token)
-    #     except BadSignature:
-    #         return None  # invalid token
-    #     # user = UserModel.query.get(data['id'])
-    #     user = db.get_or_404(UserModel, data['id'])
-    #     return user
 
