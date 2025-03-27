0aaf04a4a9fca505c5e1bce06c0968fa5213592f
Yaroslav L <yariklaz@yandex.ru>
2025-03-27 13:55:06 +0300

    Step_32: JWT authorization added

--- QuoteAPI/api/models/user.py
+++ QuoteAPI/api/models/user.py
@@ -1,10 +1,14 @@
 from passlib.apps import custom_app_context as pwd_context
-from api import db
+from api import app, db
 from config import Config
-from itsdangerous import URLSafeSerializer
-from itsdangerous import BadSignature
 import sqlalchemy.orm as so
 import sqlalchemy as sa
+# from itsdangerous import URLSafeSerializer
+# from itsdangerous import BadSignature
+import jwt
+from time import time
+
+
 
 
 
@@ -29,17 +33,33 @@ class UserModel(db.Model):
         return pwd_context.verify(password, self.password_hash)
     
     def generate_auth_token(self):
-        s = URLSafeSerializer(Config.SECRET_KEY)
-        return s.dumps({'id': self.id})
+        # s = URLSafeSerializer(Config.SECRET_KEY)
+        # return s.dumps({'id': self.id})
+        token = jwt.encode({'id': self.id, 'exp': int(time()+3600)},
+                           key=app.config['SECRET_KEY'],
+                           algorithm='HS256')
+        return token
+        # app.config['SECRET_KEY'] эквивалентно Config.SECRET_KEY
 
     @staticmethod
     def verify_auth_token(token):
-        s = URLSafeSerializer(Config.SECRET_KEY)
         try:
-            data = s.loads(token)
-        except BadSignature:
-            return None  # invalid token
-        # user = UserModel.query.get(data['id'])
+            data = jwt.decode(token, key=app.config['SECRET_KEY'], algorithms=['HS256'])
+        except Exception as e:
+            print(f'Error: {str(e)}')
+            return None
         user = db.get_or_404(UserModel, data['id'])
         return user
+
+
+    # @staticmethod
+    # def verify_auth_token(token):
+    #     s = URLSafeSerializer(Config.SECRET_KEY)
+    #     try:
+    #         data = s.loads(token)
+    #     except BadSignature:
+    #         return None  # invalid token
+    #     # user = UserModel.query.get(data['id'])
+    #     user = db.get_or_404(UserModel, data['id'])
+    #     return user
 
