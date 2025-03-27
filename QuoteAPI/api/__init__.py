21392b1410f2d9178d042790035f37e487a56e0d
Yaroslav L <yariklaz@yandex.ru>
2025-03-27 12:14:35 +0300

    Step_30: Basic Auth for author/quote POST/PUT

--- QuoteAPI/api/__init__.py
+++ QuoteAPI/api/__init__.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, g
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from flask_marshmallow import Marshmallow
+from flask_httpauth import HTTPBasicAuth
 
 
 class Base(DeclarativeBase):
@@ -15,9 +16,25 @@ app.config.from_object("config.Config")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+# Solution
+# https://stackoverflow.com/questions/59455520/flask-sqlalchemy-marshmallow-error-on-relationship-one-to-many
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
 ma.init_app(app)
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(username, password):
+    from api.models.user import UserModel
+    user = db.one_or_404(db.select(UserModel).filter_by(username=username))
+    if not user or not user.verify_password(password):
+        return False
+    # g - специальный класс flask, который хранит контекст нашего приложения
+    # нашего пользователя мы добавляем к глобальному контексту приложения
+    g.user = user
+    return True
+
 
 
 from api.handlers import author
